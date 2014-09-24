4a2bfab12e5f2ac0c42c3a79d3936cdf921df3e3
Ed Yu <edlyuu@gmail.com>
2014-09-23 23:15:59 -0700

    Skeleton HW01.hs

---
+++ HW01.hs
@@ -0,0 +1,14 @@
+{-
+ Name: Ed Yu
+ Collaborators: none
+ Notes:
+ -}
+
+module HW01 where         -- We'll learn more about this later
+
+isThisWorking :: String
+isThisWorking = "Yes"
+-- Load this file into GHCi (say, with `ghci HW01.hs`) and type
+-- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
+
+-- Put your work below.
