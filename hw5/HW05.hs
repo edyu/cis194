18e3e80aa77b1d207b5d489d38c3502bbdbbb708
Ed Yu <edlyuu@gmail.com>
2014-11-23 20:45:23 -0600

    Add example test definition.

--- hw5/HW05.hs
+++ hw5/HW05.hs
@@ -9,3 +9,10 @@ module HW05 where
 
 import Ring
 import Parser
+
+-- Exercise 1
+-- Example test definition
+intParsingWorks :: Bool
+intParsingWorks = (parse "3" == Just (3 :: Integer, "")) &&
+                  (parseRing "1 + 2 * 5" == Just (11 :: Integer)) &&
+                  (addId == (0 :: Integer))
