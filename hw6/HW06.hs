5bd04ece0a7457d57afdec916bb7a2001174d323
Ed Yu <edlyuu@gmail.com>
2014-12-05 17:08:30 -0800

    Parse JSON data.

--- hw6/HW06.hs
+++ hw6/HW06.hs
@@ -18,8 +18,12 @@ import qualified Data.Text.IO               as T
 
 -- Exercise 1
 ynToBool :: Value -> Value
-ynToBool (String "Y") = Bool   True
-ynToBool (String "N") = Bool   False
+ynToBool (String "Y") = Bool True
+ynToBool (String "N") = Bool False
 ynToBool (Object x)   = Object (fmap ynToBool x)
 ynToBool (Array x)    = Array  (fmap ynToBool x)
 ynToBool x            = x
+
+-- Exercise 2
+parseData :: B.ByteString -> Either String Value
+parseData x = fmap ynToBool $ eitherDecode x
