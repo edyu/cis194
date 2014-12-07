9cbfde5dcf2f7e4f0fb2f53c093309c63a68ff32
Ed Yu <edlyuu@gmail.com>
2014-12-06 18:35:24 -0800

    Parse the list of markets in the file.

--- hw6/HW06.hs
+++ hw6/HW06.hs
@@ -15,15 +15,32 @@ import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as T
+import qualified Data.Vector                as V
 
 -- Exercise 1
 ynToBool :: Value -> Value
 ynToBool (String "Y") = Bool True
 ynToBool (String "N") = Bool False
-ynToBool (Object x)   = Object (fmap ynToBool x)
-ynToBool (Array x)    = Array  (fmap ynToBool x)
-ynToBool x            = x
+ynToBool (Object o)   = Object (fmap ynToBool o)
+ynToBool (Array  a)   = Array  (fmap ynToBool a)
+ynToBool v            = v
 
 -- Exercise 2
 parseData :: B.ByteString -> Either String Value
-parseData x = fmap ynToBool $ eitherDecode x
+parseData bs = fmap ynToBool $ eitherDecode bs
+
+-- Exercise 3
+data Market = Market { marketname :: T.Text
+                     , x          :: Double
+                     , y          :: Double
+                     , state      :: T.Text }
+  deriving (Show, Generic)
+
+instance FromJSON Market
+
+parseMarkets :: B.ByteString -> Either String [Market]
+parseMarkets bs = case fmap fromJSON $ parseData bs :: Either String (Result [Market]) of
+                  Left  e -> Left  e
+                  Right r -> case r of
+                      (Success m) -> Right m
+                      (Error err) -> Left err
