77b5399535cf65dd9fa19920c1b898f3f1629095
Ed Yu <edlyuu@gmail.com>
2014-12-15 20:18:05 -0800

    Define a more efficient implementation of Fibonacci number.

--- hw7/HW07.hs
+++ hw7/HW07.hs
@@ -16,3 +16,7 @@ fib n = fib (n - 1) + fib (n - 2)
 fibs1 :: [Integer]
 --fibs1 = [ fib i | i <- [0..] ]
 fibs1 = map fib [0..]
+
+-- Exercise 2
+fibs2 :: [Integer]
+fibs2 = [0,1] ++ (map (\(x,y) -> x + y) $ zip fibs2 (tail fibs2))
