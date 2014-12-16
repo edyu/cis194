d0d07a2d65b65b5b9b919a31877893ae076ff358
Ed Yu <edlyuu@gmail.com>
2014-12-15 19:57:49 -0800

    Compute Fibonacci numbers with a recursive function.

--- hw7/HW07.hs
+++ hw7/HW07.hs
@@ -6,3 +6,13 @@ Notes:
 -}
 
 module HW07 where
+
+-- Exercise 1
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs1 :: [Integer]
+--fibs1 = [ fib i | i <- [0..] ]
+fibs1 = map fib [0..]
