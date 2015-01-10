9ba72ac42fdbb49023312234df374f7222679f9c
Ed Yu <edlyuu@gmail.com>
2015-01-10 23:36:50 +0900

    Detect whether a string has a certain format.

--- hw8/HW08.hs
+++ hw8/HW08.hs
@@ -6,3 +6,21 @@ Notes:
 -}
 
 module HW08 where
+
+import Data.List  (stripPrefix)
+import Data.Maybe (isJust)
+import Text.Read  (readMaybe)
+
+-- Exercise 1
+stringFitsFormat :: String -> Bool
+stringFitsFormat = isJust . go
+  where go :: String -> Maybe String
+          -- go evaluates to 'Just ""' on success, or Nothing otherwise
+        go xs = parse xs >>= done
+        parse []     = Nothing
+        parse (x:xs) = do
+            n <- readMaybe [x] :: Maybe Int
+            strip n xs
+        strip n xs = stripPrefix (take n $ repeat 'a') xs
+        done [] = Just ""
+        done xs = go xs
