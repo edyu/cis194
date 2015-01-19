068ddf9bbb339f894d21470cf60049d33708aa51
Ed Yu <edlyuu@gmail.com>
2015-01-19 15:58:44 -0800

    Compute the change in the number of armies resulting from the rolls.

--- hw8/HW08.hs
+++ hw8/HW08.hs
@@ -7,8 +7,9 @@ Notes:
 
 module HW08 where
 
-import Data.List  (stripPrefix)
+import Data.List  (sortBy, stripPrefix)
 import Data.Maybe (isJust)
+import Data.Monoid
 import Text.Read  (readMaybe)
 import Control.Monad.Random
 
@@ -44,3 +45,20 @@ type DieRoll = Int
 
 dieRoll :: StdRand DieRoll
 dieRoll = getRandomR (1, 6)
+
+-- Exercise 4
+instance Monoid ArmyCounts where
+    mempty  = ArmyCounts { attackers = 0, defenders = 0 }
+    mappend x y = ArmyCounts { attackers = (attackers x) - (attackers y),
+                               defenders = (defenders x) - (defenders y) }
+
+battleResults :: [DieRoll] -> [DieRoll] -> ArmyCounts
+battleResults as ds = initial <> (foldr fight initial dies)
+  where sortR = sortBy (flip compare)
+        dies = zip (sortR as) (sortR ds)
+        initial = ArmyCounts { attackers = (length as),
+                               defenders = (length ds) }
+        fight (a, d) ac | a <= d    = ac <> ArmyCounts { attackers = -1,
+                                                         defenders = 0 }
+                        | otherwise = ac <> ArmyCounts { attackers = 0,
+                                                         defenders = -1 }
