12da075a7c10a658256a4d62f84978ac6c1664fd
Ed Yu <edlyuu@gmail.com>
2015-01-19 14:50:13 -0800

    Simulate rolling a fair 6-sided die.

--- hw8/HW08.hs
+++ hw8/HW08.hs
@@ -10,6 +10,7 @@ module HW08 where
 import Data.List  (stripPrefix)
 import Data.Maybe (isJust)
 import Text.Read  (readMaybe)
+import Control.Monad.Random
 
 -- Exercise 1
 stringFitsFormat :: String -> Bool
@@ -31,3 +32,15 @@ specialNumbers = [ n | n <- [1..100]
                      , n `mod` 5 == 0
                      , n `mod` 7 /= 0
                  ]
+
+-- Exercise 3
+type StdRand = Rand StdGen
+
+type Army = Int
+data ArmyCounts = ArmyCounts { attackers :: Army, defenders :: Army }
+  deriving Show
+
+type DieRoll = Int
+
+dieRoll :: StdRand DieRoll
+dieRoll = getRandomR (1, 6)
