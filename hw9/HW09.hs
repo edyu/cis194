df01fb6dbc3700ce12b178870479c55b9287939e
Ed Yu <edlyuu@gmail.com>
2015-01-20 22:23:21 -0800

    Write Arbitrary instance for Mat2x2.

--- hw9/HW09.hs
+++ hw9/HW09.hs
@@ -9,6 +9,7 @@ module HW09 where
 
 import Test.QuickCheck
 
+import Control.Monad (replicateM)
 import Ring
 
 -- Exercise 1
@@ -19,3 +20,11 @@ genMod5 :: Gen Mod5
 genMod5 = do
     n <- arbitrary
     return $ mkMod n
+
+instance Arbitrary Mat2x2 where
+    arbitrary = genMat2x2
+
+genMat2x2 :: Gen Mat2x2
+genMat2x2 = do
+    [x, y, z, w] <- replicateM 4 arbitrary
+    return $ MkMat x y z w
