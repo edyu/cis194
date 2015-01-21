e541ae01cc5812eff1ddcdb306339a7cfcb0ae41
Ed Yu <edlyuu@gmail.com>
2015-01-20 17:36:00 -0800

    Write Arbitrary instance for Mod5.

--- hw9/HW09.hs
+++ hw9/HW09.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 {-
 Name: Ed Yu
 Collaborators:
@@ -6,3 +6,16 @@ Notes:
 -}
 
 module HW09 where
+
+import Test.QuickCheck
+
+import Ring
+
+-- Exercise 1
+instance Arbitrary Mod5 where
+    arbitrary = genMod5
+
+genMod5 :: Gen Mod5
+genMod5 = do
+    n <- arbitrary
+    return $ mkMod n
