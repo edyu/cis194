8d9a46ed3026633c5936383438c3e86a474df3a5
Ed Yu <edlyuu@gmail.com>
2015-01-21 11:15:22 -0800

    Encode the properties that all rings should have.

--- hw9/HW09.hs
+++ hw9/HW09.hs
@@ -36,3 +36,34 @@ shrinkMat2x2 (MkMat a b c d) = [MkMat x y z w | x <- shrinkIntegral a
                                               , y <- shrinkIntegral b
                                               , z <- shrinkIntegral c
                                               , w <- shrinkIntegral d]
+
+-- Exercise 3
+prop_1 :: (Ring a, Eq a) => a -> a -> a -> Bool
+prop_1 a b c = add (add a b) c == add a (add b c)
+
+prop_2 :: (Ring a, Eq a) => a -> Bool
+prop_2 a = add a addId == a && add addId a == a
+
+prop_3 :: (Ring a, Eq a) => a -> Bool
+prop_3 a = add a (addInv a) == add (addInv a) a
+
+prop_4 :: (Ring a, Eq a) => a -> Bool
+prop_4 a = add a (addInv a) == addId && add (addInv a) a == addId
+
+prop_5 :: (Ring a, Eq a) => a -> a -> Bool
+prop_5 a b = add a b == add b a
+
+prop_6 :: (Ring a, Eq a) => a -> a -> a -> Bool
+prop_6 a b c = mul (mul a b) c == mul a (mul b c)
+
+prop_7 :: (Ring a, Eq a) => a -> Bool
+prop_7 a = mul a mulId == a && mul mulId a == a
+
+prop_8 :: (Ring a, Eq a) => a -> a -> a -> Bool
+prop_8 a b c = mul a (add b c) == add (mul a b) (mul a c)
+
+prop_9 :: (Ring a, Eq a) => a -> a -> a -> Bool
+prop_9 a b c = mul (add b c) a == add (mul b a) (mul c a)
+
+prop_10 :: (Ring a, Eq a) => a -> a -> a -> Bool
+prop_10 a b c = if a == b && b == c then a == c else a /= b || b /= c
